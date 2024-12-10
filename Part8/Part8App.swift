74fa803365d7b2ec57c16171286f59740b708a79
silicagel <ksk19981004@gmail.com>
2024-12-11 06:51:42 +0900

    Initial Commit

---
+++ Part8/Part8App.swift
@@ -0,0 +1,17 @@
+//
+//  Part8App.swift
+//  Part8
+//
+//  Created by silicagel on 2024/12/11.
+//
+
+import SwiftUI
+
+@main
+struct Part8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
