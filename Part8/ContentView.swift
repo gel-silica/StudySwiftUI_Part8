f482f5d8b3a6cbd0d0d3625e20298324a6c15e7d
silicagel <ksk19981004@gmail.com>
2024-12-12 06:19:04 +0900

    @State,@Binding

--- Part8/ContentView.swift
+++ Part8/ContentView.swift
@@ -8,16 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count = 0
+    @State var isShowContentView2 = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack{
+                HStack {
+                    Button("-") {
+                        count -= 1
+                    }
+                    Text("Counter: \(count)")
+                    Button("+") {
+                        count += 1
+                    }
+                }
+                .padding()
+                Button("ContentView2へ") {
+                    isShowContentView2 = true
+                }
+            }
+            .font(.title)
+            .sheet(isPresented: $isShowContentView2) {
+                ContentView2(tenCount: $count)
+            }
         }
-        .padding()
+        
     }
 }
+struct ContentView2: View {
+    @Binding var tenCount: Int
+
+    var body: some View {
+        Button("+10") {
+            tenCount += 10
+        }
+    }
+}
+
+//#Preview {
+//    ContentView2()
+//}
 
 #Preview {
     ContentView()
